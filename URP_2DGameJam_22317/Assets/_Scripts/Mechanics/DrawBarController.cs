a5487d0fab0dde43fa0bb359187e6e8b0f07d5be
KANO-NE <807573653@qq.com>
2025-03-20 16:28:47 +0800

    拉杆做了一半
    有事

---
+++ URP_2DGameJam_22317/Assets/_Scripts/Mechanics/DrawBarController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+ /// <summary>
+ ///注释
+ /// <summary>
+
+public class DrawBarController : MonoBehaviour
+{
+    public Transform door;
+}
