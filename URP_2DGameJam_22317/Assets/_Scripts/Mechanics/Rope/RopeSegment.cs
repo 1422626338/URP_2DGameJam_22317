2889cbeea16581f66c835dc8d1066dadde21029e
1422626338 <1422626338@qq.com>
2025-03-22 17:42:20 +0800

    锁链测试

--- URP_2DGameJam_22317/Assets/_Scripts/Mechanics/Rope/RopeSegment.cs
+++ URP_2DGameJam_22317/Assets/_Scripts/Mechanics/Rope/RopeSegment.cs
@@ -1,26 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RopeSegment : MonoBehaviour
 {
     public GameObject connectedAbove, connectedBelow;
 
-    // Start is called before the first frame update
     void Start()
     {
-        connectedAbove = GetComponent<HingeJoint2D>().connectedBody.gameObject;
-        RopeSegment aboveSegment = connectedAbove.GetComponent<RopeSegment>();
-        if (aboveSegment != null)
+        // 确保上方连接物体存在且包含HingeJoint2D
+        if (GetComponent<HingeJoint2D>().connectedBody != null)
         {
-            aboveSegment.connectedBelow = gameObject;
-            float spriteBottom = connectedAbove.GetComponent<SpriteRenderer>().bounds.size.y;
-            GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, spriteBottom - 1);
-        }
-        else
-        {
-            GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, 0);
+            connectedAbove = GetComponent<HingeJoint2D>().connectedBody.gameObject;
+            RopeSegment aboveSegment = connectedAbove.GetComponent<RopeSegment>();
+
+            if (aboveSegment != null)
+            {
+                // 建立双向连接
+                aboveSegment.connectedBelow = gameObject;
+
+                // 动态计算锚点位置
+                SpriteRenderer aboveSprite = connectedAbove.GetComponent<SpriteRenderer>();
+                if (aboveSprite != null)
+                {
+                    float spriteHeight = aboveSprite.bounds.size.y;
+                    // 将锚点设置在上方物体的底部中心
+                    GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, -spriteHeight * 0.5f);
+                }
+            }
+            else
+            {
+                // 如果是顶部的挂钩，锚点设为默认
+                GetComponent<HingeJoint2D>().connectedAnchor = Vector2.zero;
+            }
         }
     }
 }
-
