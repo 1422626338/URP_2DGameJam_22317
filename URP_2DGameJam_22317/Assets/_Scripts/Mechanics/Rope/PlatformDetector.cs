f370bcb305c7992c3218e9b40d39a17cc774a071
1422626338 <1422626338@qq.com>
2025-03-25 21:52:43 +0800

    不做处理

--- URP_2DGameJam_22317/Assets/_Scripts/Mechanics/Rope/PlatformDetector.cs
+++ URP_2DGameJam_22317/Assets/_Scripts/Mechanics/Rope/PlatformDetector.cs
@@ -13,7 +13,7 @@ public class PlatformDetector : MonoBehaviour
         if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
         {
             hasPlayer = true;
-            //TODO:更改父物体，防止出现抖动
+            //HACK:更改父物体，防止出现抖动
             collision.transform.parent = this.transform;
         }
     }
@@ -25,11 +25,31 @@ public class PlatformDetector : MonoBehaviour
         {
             hasPlayer = false;
             //TODO:退出父物体
-            if(collision.transform.parent != null)
+            if (collision.transform.parent != null)
             {
-                collision.transform.parent = null;
+                collision.transform.parent = null;  //这里报错不太敏感，先不做处理
             }
         }
     }
+    //HACK:
+    #region 如果角色站在锁链平台停止运行游戏，发生错误时，使用
+    //private IEnumerator DetachPlayerAfterDelay(Transform playerTransform)
+    //{
+    //    yield return null;  // 等待一帧
+    //    if (playerTransform != null)
+    //    {
+    //        playerTransform.parent = null;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
+    //    {
+    //        hasPlayer = false;
+    //        StartCoroutine(DetachPlayerAfterDelay(collision.transform));
+    //    }
+    //}
+    #endregion
 }
 
