cd1389cea9256d18f898d486eb5a5128a5de1fbd
KANO-NE <807573653@qq.com>
2025-05-05 20:31:35 +0800

    1_5开头

--- URP_2DGameJam_22317/Assets/_Scripts/level/Level1_5.cs
+++ URP_2DGameJam_22317/Assets/_Scripts/level/Level1_5.cs
@@ -6,7 +6,41 @@ using UnityEngine;
  ///注释
  /// <summary>
 
-public class Level1_5 : MonoBehaviour
+public class Level1_5 : BaseLevel
 {
- 
+    private void Awake()
+    {
+
+    }
+    private void OnEnable()
+    {
+        curTimeCount = timeCount;
+        panelCnt = 0;
+        isFail = false;
+        LevelManager.Instance.SetPlayerPoint(P1SpawnPoint.position, P2SpawnPoint.position);
+        LevelManager.Instance.p1.SetActive(true);
+        LevelManager.Instance.p2.SetActive(true);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (curTimeCount > 0)
+        {
+            curTimeCount -= Time.deltaTime;
+        }
+
+        if (curTimeCount < 0)
+        {
+            isFail = true;
+
+        }
+
+        if (isFail && panelCnt == 0)
+        {
+            panelCnt = 1;
+            Debug.Log("游戏失败");
+            GameOverEvent.RaiseEvent(null, this);
+        }
+    }
 }
