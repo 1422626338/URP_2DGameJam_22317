2762f707811f9bca94aad01454121624a5f00440
1422626338 <1422626338@qq.com>
2025-05-13 09:20:14 +0800

    修改部分关卡设计，控制台的材质摩擦力设为0

--- URP_2DGameJam_22317/Assets/_Scripts/level/Level1_1.cs
+++ URP_2DGameJam_22317/Assets/_Scripts/level/Level1_1.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 /// <summary>
 ///注释
@@ -9,10 +8,9 @@ using UnityEngine.Rendering.Universal;
 
 public class Level1_1 : BaseLevel
 {
-
     private void Awake()
     {
-        
+
     }
     private void OnEnable()
     {
@@ -20,27 +18,27 @@ public class Level1_1 : BaseLevel
         panelCnt = 0;
         isFail = false;
         isTimeOut = false;
-        LevelManager.Instance.SetPlayerPoint(P1SpawnPoint.position , P2SpawnPoint.position);
+        LevelManager.Instance.SetPlayerPoint(P1SpawnPoint.position, P2SpawnPoint.position);
         LevelManager.Instance.p1.SetActive(true);
         LevelManager.Instance.p2.SetActive(true);
         LevelManager.Instance.levelPos = curLevelPos;
     }
 
-     protected override void Update()
+    protected override void Update()
     {
         base.Update();
-        if (curTimeCount > 0 && isTimeOut)
+        if (curTimeCount > 0 && !isTimeOut)
         {
             curTimeCount -= Time.deltaTime;
         }
 
-        if (curTimeCount < 0 && isTimeOut)
+        if (curTimeCount < 0 && !isTimeOut)
         {
             isFail = true;
-           
+
         }
 
-        if(isFail && panelCnt == 0)
+        if (isFail && panelCnt == 0)
         {
             panelCnt = 1;
             Debug.Log("游戏失败");
